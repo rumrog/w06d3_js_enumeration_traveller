bc7294885b820e7b7975170edfb3d77f338f9f51
Rumen Roger <rumenroger@gmail.com>
2020-06-18 00:16:00 +0100

    Traveller: should be able to get the journeys end locations

--- hw_enumeration_traveller/models/traveller.js
+++ hw_enumeration_traveller/models/traveller.js
@@ -8,8 +8,10 @@ Traveller.prototype.getJourneyStartLocations = function() {
   });
 };
 
-Traveller.prototype.getJourneyEndLocations = function () {
-
+TTraveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
